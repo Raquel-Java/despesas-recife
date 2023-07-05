ff0bb036a2876917ea09b4aef729570844cb0035
Raquel-Java <82677484+Raquel-Java@users.noreply.github.com>
2023-07-05 01:12:38 -0300

    Componentes
    Criação, alteração, edição e deleção de despesas

--- frontend/src/app/components/views/expense/expense-delete/expense-delete.component.ts
+++ frontend/src/app/components/views/expense/expense-delete/expense-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Expense } from '../expense.model';
+import { ExpenseService } from '../expense.service';
 
 @Component({
   selector: 'app-expense-delete',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseDeleteComponent implements OnInit {
 
-  constructor() { }
+  expense: Expense = {
+    ano: '',
+    mes: '',
+    codOrgao: '',
+    nomeOrgao: '',
+    subElemento: '',
+    valorEmpenhado: 0,
+    valorLiquido: 0,
+    valorPago: 0
+  }
+
+  constructor(private expenseService: ExpenseService, 
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    const id= this.route.snapshot.paramMap.get('id')
+    this.expenseService.readById(id).subscribe(expense=>{
+      this.expense = expense
+    });
+
+  }
+
+  deleteExpense(): void {
+    this.expenseService.delete(this.expense.id).subscribe(() => {
+      this.expenseService.showMessege('Despesa deletada com sucesso!');
+      this.router.navigate(['/expense']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/expense']);
+
   }
 
 }
--- frontend/src/app/components/views/expense/expense-update/expense-update.component.ts
+++ frontend/src/app/components/views/expense/expense-update/expense-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Expense } from '../expense.model';
+import { ExpenseService } from '../expense.service';
 
 @Component({
   selector: 'app-expense-update',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseUpdateComponent implements OnInit {
 
-  constructor() { }
+  expense: Expense = {
+    ano: '',
+    mes: '',
+    codOrgao: '',
+    nomeOrgao: '',
+    subElemento: '',
+    valorEmpenhado: 0,
+    valorLiquido: 0,
+    valorPago: 0
+  }
+
+  constructor(private expenseService: ExpenseService, 
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    const id= this.route.snapshot.paramMap.get('id')
+    this.expenseService.readById(id).subscribe(expense=>{
+      this.expense = expense
+    });
+
+  }
+
+  updateExpense(): void {
+    this.expenseService.update(this.expense).subscribe(() => {
+      this.expenseService.showMessege('Despesa atualizada com sucesso!');
+      this.router.navigate(['/expense']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/expense']);
+
   }
 
 }
